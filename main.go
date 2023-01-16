f65d641a16936f9a783ef6ffe87a6a466e230a9c
pablomoreira <pablolokekieran@gmail.com>
2023-01-16 11:29:04 -0300

    Changes to be committed: Algunas ideas nuevas con contextos

--- main.go
+++ main.go
@@ -6,30 +6,49 @@ import (
 	"os"
 	"time"
 )
-
-func main() {
-	line := make(chan string)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	go tail("file.log", line, ctx)
-
-	log.Print(<-line)
+type person struct {
+    filename string
+	line string
+    chang bool
+	FileObjet os.File
 }
 
-func tail(filename string, line chan string, ctx context.Context) {
+func main() {
+	//line := make(chan string)
+	ctx, cancel := context.WithCancel(context.Background())
+	//defer cancel()
+	
+	go tail("file.log", cancel, time.Millisecond*100)
+
+	//log.Print(<-line)
+	for _loop := true; _loop == true; {
+		select {
+		case <-ctx.Done():
+			log.Print("Done")
+			_loop = false
+		default:
+			
+		}
+		time.Sleep(time.Millisecond * 1)
+	}
+}
+
+func tail(filename string, cancel context.CancelFunc, t_sleep time.Duration) {
 	//File, err := os.OpenFile(filename, os.O_RDONLY, 644)
 	var Info os.FileInfo
 	var err error
-
-	for true {
+	time.Sleep(t_sleep)
+	for _loop := true; _loop == true; {
 		Info, err = os.Stat(filename)
 
 		if err != nil {
-			log.Panic()
+			log.Panic(err)
 		}
 		log.Printf("%d\n", Info.Size())
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Second * 1)
+		_loop = false
+		cancel()
 	}
-	line <- "End"
-	ctx.Done()
 }
+
+func check
