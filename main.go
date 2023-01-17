9314177fb4a6144befd6eaea64bdf4bd2fe26dc3
pablomoreira <pablolokekieran@gmail.com>
2023-01-17 11:30:27 -0300

    Estructura para hacer chekear el archivo con un canal para detectar los cambios de tamaño

--- main.go
+++ main.go
@@ -13,25 +13,35 @@ type Tail struct {
 	change    bool
 	FileObjet os.File
 	time      time.Duration
+	size      uint64
+	test      chan bool
 }
 
 func (tail *Tail) Check() {
 	var Info os.FileInfo
 	var err error
+	tail.test = make(chan bool)
 	for _loop := true; _loop == true; {
 		Info, err = os.Stat(tail.filename)
-
 		if err != nil {
 			log.Panic(err)
 		}
-		log.Printf("%d\n", Info.Size())
+		//log.Printf("%d\n", Info.Size())
+		_size := uint64(Info.Size())
+		if tail.size != _size {
+			tail.size = _size
+			tail.change = true
+			tail.test <- true
+
+		} else {
+		}
 		time.Sleep(tail.time)
 
 	}
 }
 
 func main() {
-	FileWatch := Tail{filename: "file.log", time: time.Second * 1}
+	FileWatch := Tail{filename: "file.log", time: time.Second * 1, size: 0}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -41,14 +51,17 @@ func main() {
 
 	//log.Print(<-line)
 	for _loop := true; _loop == true; {
+		time.Sleep(time.Millisecond * 10)
 		select {
 		case <-ctx.Done():
 			log.Print("Done")
-			//_loop = false
+		case <-FileWatch.test:
+			log.Printf("change %v size %v", FileWatch.change, FileWatch.size)
+			FileWatch.change = false
 		default:
-			log.Print(FileWatch.change)
+
 		}
-		time.Sleep(time.Millisecond * 1000)
+
 	}
 
 }
