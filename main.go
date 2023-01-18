2aee5b262a0f887ad94b6b16c62696be3aff1764
pablomoreira <pablolokekieran@gmail.com>
2023-01-18 11:42:51 -0300

    modified:   main.go 	primer bosquejo de lectura

--- main.go
+++ main.go
@@ -10,17 +10,30 @@ import (
 type Tail struct {
 	filename  string
 	line      string
-	change    bool
-	FileObjet os.File
+	change    chan bool
+	FileObjet *os.File
 	time      time.Duration
 	size      uint64
-	test      chan bool
+	posi      uint64
+}
+
+// Read last line
+func (tail *Tail) ReadLine() {
+	var err error
+	tail.FileObjet, err = os.OpenFile(tail.filename, os.O_RDONLY, 600)
+	if err != nil {
+		log.Panic(err)
+	}
+	tail.FileObjet.Seek(-1, 2)
+	var b []byte
+	r, e := tail.FileObjet.Read(b)
+	log.Print(b, e, r)
 }
 
 func (tail *Tail) Check() {
 	var Info os.FileInfo
 	var err error
-	tail.test = make(chan bool)
+	tail.change = make(chan bool)
 	for _loop := true; _loop == true; {
 		Info, err = os.Stat(tail.filename)
 		if err != nil {
@@ -30,13 +43,13 @@ func (tail *Tail) Check() {
 		_size := uint64(Info.Size())
 		if tail.size != _size {
 			tail.size = _size
-			tail.change = true
-			tail.test <- true
+
+			tail.change <- true
+			tail.ReadLine()
 
 		} else {
 		}
 		time.Sleep(tail.time)
-
 	}
 }
 
@@ -55,15 +68,11 @@ func main() {
 		select {
 		case <-ctx.Done():
 			log.Print("Done")
-		case <-FileWatch.test:
+		case <-FileWatch.change:
 			log.Printf("change %v size %v", FileWatch.change, FileWatch.size)
-			FileWatch.change = false
+			//FileWatch.change = false
 		default:
 
 		}
-
 	}
-
 }
-
-//func check
